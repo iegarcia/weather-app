c49cf7761d632f83587dbbb4f7ff510e416f6b28
iegarcia <nachogarcia003@gmail.com>
2020-03-16 17:41:31 -0300

    Comienza nuevo proyecto!!!

--- src/index.js
+++ src/index.js
@@ -1,30 +1,12 @@
-// const element = document.createElement("h1");
-// element.innerText = "Probando 1 2 3!";
-// const container = document.getElementById("app");
-// container.appendChild(element);
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import * as serviceWorker from './serviceWorker';
 
-// Hecho con react seria
-import React from "react"; // document.createElement
-import ReactDom from "react-dom"; //container.appendChild
+ReactDOM.render(<App />, document.getElementById('root'));
 
-const elemento = <h1>Probando 1 2 3!</h1>;
-// React.createElement("tag", {atributo}, "contenido"),
-
-const jsx = ( //JSX es la forma de escribir código mas fácil de leer, compacto. Una sola variable puede llegar a contener todo un sector de la pagina
-  <div>
-    <h1>Hola</h1>
-    <p>Que tal!</p>
-  </div>
-);
-
-//Hecho de la forma tradicional
-const element = React.createElement(
-  "div",
-  {},
-  React.createElement("h1", {}, "Hola"), //Pueden contener variables, expresiones y si las mismas terminan en false no se muestran
-  React.createElement("p", {}, "Que tal!")
-);
-
-const container = document.getElementById("app");
-
-ReactDom.render(element, container); //Reemplaza appendChild y se le pasan como parámetro "que" renderizar y "donde"
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
